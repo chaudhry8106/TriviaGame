f3d3b27041de892daca277b5bb598ec6f0f03aed
Muhammad <mchaudh2@uncc.edu>
2017-07-07 21:55:34 -0400

    adding css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,65 @@
-var timer = setTimeout(timeLeft, 1000 * 1)
+var seconds = 121
+var intervalId;
 
-function counting() {
-	$("#time-left").append("<h3>" + timer + "</h3>");
-  	console.log("10 seconds left");
-}
-
-counting();
-
-
-// var countDown = setTimeout(counting, 1000 * 120);
 
 function timeLeft() {
 
-	for (var i = 120; i > 0; i--) {
-		counting[i];
+	seconds--;
+	$("#time-left").html("<h3>" + seconds + "</h3>");
+	if (seconds === 0) {
+		alert("Time uP!!");
+		clearInterval(intervalId);
 	}
 
 }
 
 timeLeft();
+
+function timer() {
+      intervalId = setInterval(timeLeft, 1000);
+    }
+
+timer();
+
+
+
+
+    var numQues = 5;
+    var numChoi = 3;
+    var answers = new Array(5);
+        answers[0] = "doesn't like";
+        answers[1] = "don't come";
+        answers[2] = "come";
+        answers[3] = "don't";
+        answers[4] = "doesn't make";
+
+          function getScore(form) {
+      
+       var score = 0;
+      var currElt;
+      var currSelection;
+      for (i=0; i<numQues; i++) {
+        currElt = i*numChoi;
+        answered=false; 
+        for (j=0; j<numChoi; j++) {
+          currSelection = form.elements[currElt + j];
+          if (currSelection.checked) {
+            answered=true;
+            if (currSelection.value == answers[i]) {
+              score++;
+              break;
+            }
+          }
+        }
+        if (answered ===false){alert("Do answer all the questions, Please") ;return false;}
+      }
+    
+      var scoreper = Math.round(score/numQues*100);
+      form.percentage.value = scoreper + "%";
+      form.mark.value=score;
+    
+    
+    }
+
+// $("getScore").on("click", scoreper);
+// $("clear").on("click", returnScore);
